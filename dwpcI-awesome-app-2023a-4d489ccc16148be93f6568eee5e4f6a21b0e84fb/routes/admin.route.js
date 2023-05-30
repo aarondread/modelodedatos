7b9d446491792b9cd8f2f8d0a5f7f11577f8d50b
aarondread <106945243+aarondread@users.noreply.github.com>
2023-05-29 21:58:53 -0600

    Add files via upload

---
+++ dwpcI-awesome-app-2023a-4d489ccc16148be93f6568eee5e4f6a21b0e84fb/routes/admin.route.js
@@ -0,0 +1,32 @@
+// Importando el enrutador de express
+import { Router } from 'express';
+// Importando el gestor de rutas
+import path from 'path';
+import { ROOT_DIR } from '../helpers/paths.js';
+
+// Creando una instancia del enrutador de express
+const router = Router();
+
+// Datos en memoria volatil
+export const products = [];
+
+// GET /admin/add-product
+router.get('/add-product', (req, res, next) => {
+  // Servimos el formulario
+  console.log("📢 Sirviendo formulario...");
+  res.sendFile(path.join(ROOT_DIR, 'views','add-product.html'));
+});
+
+// POST /admin/add-product
+router.post('/add-product', (req, res) => {
+  // Realizaremos la desestructuracion de
+  // "name" de la petición
+  const { title } = req.body;
+  // Agregamos el dato en la base de datos
+  products.push(title);
+  // Redireccionando
+  res.redirect('/');
+});
+
+// Exportando el enrutador admin
+export default router;
